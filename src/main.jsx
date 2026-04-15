ee575860637cb07638963a9c4a8a15607cffc977
Mohammed Muzamiluddin <84442905+MuzammilCodes@users.noreply.github.com>
2026-04-15 18:51:24 +0530

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <BrowserRouter>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
