e5ab804d4d5eedadec503a7220f562a351a084b8
Mohammed Muzamiluddin <84442905+MuzammilCodes@users.noreply.github.com>
2026-04-15 18:48:07 +0530

    revert 2

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,36 +1,39 @@
 import { Helmet } from "react-helmet-async";
+
 export default function SEO({ title, description, keywords, faq }) {
   return (
     <>
-      {" "}
       <Helmet>
-        {" "}
-        <title>{title}</title> <meta name="description" content={description} />{" "}
-        <meta name="keywords" content={keywords} />{" "}
-        <meta name="robots" content="index, follow" />{" "}
-      </Helmet>{" "}
-      {/* Article Schema */}{" "}
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="robots" content="index, follow" />
+      </Helmet>
+
+      {/* Article Schema */}
       <script type="application/ld+json">
-        {" "}
         {JSON.stringify({
           "@context": "https://schema.org",
           "@type": "Article",
           headline: title,
           description: description,
-          author: { "@type": "Person", name: "Muzammil" },
-        })}{" "}
-      </script>{" "}
-      {/* FAQ Schema */}{" "}
+          author: {
+            "@type": "Person",
+            name: "Muzammil"
+          }
+        })}
+      </script>
+
+      {/* FAQ Schema */}
       {faq && (
         <script type="application/ld+json">
-          {" "}
           {JSON.stringify({
             "@context": "https://schema.org",
             "@type": "FAQPage",
-            mainEntity: faq,
-          })}{" "}
+            mainEntity: faq
+          })}
         </script>
-      )}{" "}
+      )}
     </>
   );
 }
