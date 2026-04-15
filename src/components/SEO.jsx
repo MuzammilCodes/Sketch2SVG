f9277ad217e2e718474635d194c412a52f9dd861
Mohammed Muzamiluddin <84442905+MuzammilCodes@users.noreply.github.com>
2026-04-15 18:45:31 +0530

    revert seo file change

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,46 +1,36 @@
 import { Helmet } from "react-helmet-async";
-
 export default function SEO({ title, description, keywords, faq }) {
-  const articleSchema = {
-    "@context": "https://schema.org",
-    "@type": "Article",
-    headline: title,
-    description: description,
-    author: {
-      "@type": "Person",
-      name: "Muzammil"
-    },
-    publisher: {
-      "@type": "Organization",
-      name: "Sketch2SVG"
-    }
-  };
-
-  const faqSchema = faq && {
-    "@context": "https://schema.org",
-    "@type": "FAQPage",
-    mainEntity: faq
-  };
-
   return (
-    <Helmet>
-      {/* Basic SEO */}
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
-      <meta name="robots" content="index, follow" />
-
-      {/* Article Schema */}
+    <>
+      {" "}
+      <Helmet>
+        {" "}
+        <title>{title}</title> <meta name="description" content={description} />{" "}
+        <meta name="keywords" content={keywords} />{" "}
+        <meta name="robots" content="index, follow" />{" "}
+      </Helmet>{" "}
+      {/* Article Schema */}{" "}
       <script type="application/ld+json">
-        {JSON.stringify(articleSchema)}
-      </script>
-
-      {/* FAQ Schema */}
+        {" "}
+        {JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "Article",
+          headline: title,
+          description: description,
+          author: { "@type": "Person", name: "Muzammil" },
+        })}{" "}
+      </script>{" "}
+      {/* FAQ Schema */}{" "}
       {faq && (
         <script type="application/ld+json">
-          {JSON.stringify(faqSchema)}
+          {" "}
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "FAQPage",
+            mainEntity: faq,
+          })}{" "}
         </script>
-      )}
-    </Helmet>
+      )}{" "}
+    </>
   );
 }
