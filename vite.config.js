82190259b02a3c1ef73614a6453c0ccef99a646f
Mohammed Muzamiluddin <84442905+MuzammilCodes@users.noreply.github.com>
2026-04-16 11:02:16 +0530

    Add prerender plugin with specified routes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      routes: [
+        '/', 
+        '/blog/sketch-to-svg-guide'
+      ],
+    }),
+  ],
   build: {
     outDir: 'dist',
   },
